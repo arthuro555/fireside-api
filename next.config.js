d94a56b784287279e0375da314d5b8be009bfaa1
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2023-03-11 10:52:34 +0100

    Add anti-cache headers to the API

--- next.config.js
+++ next.config.js
@@ -5,12 +5,15 @@ const nextConfig = {
     {
       source: "/api/(.*)",
       headers: [
+        // Ensure other website (web games) can query the API
         { key: "Access-Control-Allow-Origin", value: "*" },
         {
           key: "Access-Control-Allow-Methods",
           value: "GET,HEAD,POST,OPTIONS,DELETE",
         },
         { key: "Access-Control-Max-Age", value: "86400" },
+        // Ensure Cloudflare & Vercel do not cache API responses
+        { key: "Cache-Control", value: "no-store" },
       ],
     },
   ],
