7ce08fbe078edc93e72be4bbcbe43ff7755ef87f
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2023-03-11 09:55:31 +0100

    Add CORS headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  headers: async () => [
+    {
+      source: "/api/(.*)",
+      headers: [
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,HEAD,POST,OPTIONS,DELETE",
+        },
+        { key: "Access-Control-Max-Age", value: "86400" },
+      ],
+    },
+  ],
   experimental: {
     appDir: true,
     typedRoutes: true,
